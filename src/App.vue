654c7fd2ca3dad39b3180caade8c75f451a885eb
jahiker <rojasjahiker@gmail.com>
2022-07-30 19:16:17 -0400

    composition api component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-  import HelloWorld from './components/HelloWorld.vue'
-  import TheWelcome from './components/TheWelcome.vue'
+import UserRepositories from './components/UserRepositories.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="I just did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <UserRepositories />
   </main>
 </template>
 
---
+++ src/components/UserRepositories.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>UserRepositories</div>
+</template>
+
+<script>
+    import { onMounted } from 'vue';
+    export default {
+        setup() {
+            onMounted(() => {
+                console.log('onMounted')
+            }) 
+        },
+    }
+</script>
