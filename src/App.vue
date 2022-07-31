f71043e8eea8120d272578b157eae149bcb28d95
jahiker <rojasjahiker@gmail.com>
2022-07-30 23:47:31 -0400

    ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,14 @@
-<script setup>
-import UserRepositories from './components/UserRepositories.vue';
-</script>
-
 <template>
-  <main>
-    <UserRepositories />
-  </main>
+  <Home />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  import Home from "./components/Home.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  export default {
+    name: "App",
+    components: {
+      Home,
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>{{ text }}</h1>
+  <p>{{ counter }}</p>
+  <p>{{ obj }}</p>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+  export default {
+    setup() {
+      const text = ref("Hola Vue");
+      const counter = ref(0);
+      const obj = reactive({
+        counter: 0
+      })
+
+      setInterval(() => counter.value++, 1000);
+      setInterval(() => obj.counter++, 1000);
+
+      return {
+        text,
+        counter,
+        obj
+      }
+    }
+  }
+</script>
